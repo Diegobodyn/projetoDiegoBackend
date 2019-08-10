2e02da9f3ba110ccc0df55220a2ebfefb7d94c90
Diego Henrique Silva <diegosilvadhs@gmail.com>
2019-08-10 16:35:08 -0300

    JPA repository, dependency injection, database seeding DiegoH

---
+++ src/main/java/com/diegohenrique/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.diegohenrique.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.diegohenrique.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
