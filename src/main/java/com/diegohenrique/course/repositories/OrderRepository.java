4b828ccebf3e74a71ff320bef79ae9866414b341
Diego Henrique Silva <diegosilvadhs@gmail.com>
2019-08-26 21:40:25 -0300

    Order, Instant, ISO 8601 - PART 2

---
+++ src/main/java/com/diegohenrique/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.diegohenrique.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.diegohenrique.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
