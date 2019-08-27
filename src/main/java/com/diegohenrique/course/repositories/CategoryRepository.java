70d50cf0b4420f4033c4543fe7e8c8d38b2e5f35
Diego Henrique Silva <diegosilvadhs@gmail.com>
2019-08-26 22:56:39 -0300

    Category

---
+++ src/main/java/com/diegohenrique/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.diegohenrique.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.diegohenrique.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
