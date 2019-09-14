db60b72fb8aad3dcea9f6bd8c1fc4330d5ef5491
Diego Henrique Silva <diegosilvadhs@gmail.com>
2019-09-14 16:47:24 -0300

    Desafio Category Challenge: category CRUD

--- src/main/java/com/diegohenrique/course/services/CategoryService.java
+++ src/main/java/com/diegohenrique/course/services/CategoryService.java
@@ -3,11 +3,17 @@ package com.diegohenrique.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.diegohenrique.course.entities.Category;
 import com.diegohenrique.course.repositories.CategoryRepository;
+import com.diegohenrique.course.services.exceptions.DatabaseException;
+import com.diegohenrique.course.services.exceptions.ResourceNotFoundException;
 
 
 @Service
@@ -23,4 +29,35 @@ public class CategoryService {
 		Optional<Category> obj = repository.findById(id);
 		return obj.get();
 	}
+	
+	// Diego alone!  ---------------------
+	public Category insert(Category obj) {
+		return repository.save(obj);
+		
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	} 
+	
+	public Category update(Long id, Category obj) {
+		try { 
+		Category entity = repository.getOne(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+		} catch ( EntityNotFoundException e) {
+		 throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(Category entity, Category obj) {
+		entity.setName(obj.getName());
+
+	} 
 }
