7c2e96e0a8b401abca9fead95e08b8d4a6c86841
Diego Henrique Silva <diegosilvadhs@gmail.com>
2019-09-14 14:17:30 -0300

    User delete

--- src/main/java/com/diegohenrique/course/resources/UserResource.java
+++ src/main/java/com/diegohenrique/course/resources/UserResource.java
@@ -3,17 +3,15 @@ package com.diegohenrique.course.resources;
 import java.net.URI;
 import java.util.List;
 
-import javax.servlet.Servlet;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.ServletRequestDataBinderFactory;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.diegohenrique.course.entities.User;
@@ -32,7 +30,7 @@ public class UserResource {
 		return ResponseEntity.ok().body(list);
 	}
 	
-	@GetMapping(value = "/{id}" )
+	@GetMapping(value = "/{id}")
 	public ResponseEntity<User> findById(@PathVariable  Long id){
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
@@ -46,4 +44,9 @@ public class UserResource {
 		return ResponseEntity.created(uri).body(obj);
 	}
 	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	} 	
 }
