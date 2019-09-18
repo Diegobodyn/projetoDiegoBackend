ce0120f2f1d33c839a9a90702c058035d1397f70
Diego Henrique Silva <diegosilvadhs@gmail.com>
2019-09-18 08:16:43 -0300

    Desafio final Diego

--- src/main/java/com/diegohenrique/course/resources/ProductResource.java
+++ src/main/java/com/diegohenrique/course/resources/ProductResource.java
@@ -1,13 +1,19 @@
 package com.diegohenrique.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.diegohenrique.course.entities.Product;
 import com.diegohenrique.course.services.ProductService;
@@ -31,5 +37,24 @@ public class ProductResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	@PostMapping
+	public ResponseEntity<Product> insert(@RequestBody Product dto){
+		Product newDto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				  .buildAndExpand(newDto.getId()).toUri() ;
+		return ResponseEntity.created(uri).body(newDto);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product dto){
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
+	}
 	
 }
--- src/main/java/com/diegohenrique/course/services/ProductService.java
+++ src/main/java/com/diegohenrique/course/services/ProductService.java
@@ -3,11 +3,17 @@ package com.diegohenrique.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.diegohenrique.course.entities.Product;
 import com.diegohenrique.course.repositories.ProductRepository;
+import com.diegohenrique.course.services.exceptions.DatabaseException;
+import com.diegohenrique.course.services.exceptions.ResourceNotFoundException;
 
 
 @Service
@@ -23,4 +29,36 @@ public class ProductService {
 		Optional<Product> obj = repository.findById(id);
 		return obj.get();
 	}
+	
+	// Diego alone!  ---------------------
+		public Product insert(Product obj) {
+			return repository.save(obj);
+			
+		}
+		
+		public void delete(Long id) {
+			try {
+				repository.deleteById(id);
+			}catch (EmptyResultDataAccessException e) {
+				throw new ResourceNotFoundException(id);
+			}catch (DataIntegrityViolationException e) {
+				throw new DatabaseException(e.getMessage());
+			}
+		} 
+		
+		public Product update(Long id, Product obj) {
+			try { 
+				Product entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+			} catch ( EntityNotFoundException e) {
+			 throw new ResourceNotFoundException(id);
+			}
+		}
+
+		private void updateData(Product entity, Product obj) {
+			entity.setName(obj.getName());
+
+		} 
+	
 }
