a32bb29362f14b35ee15aadbee678d445ebfae1e
Diego Henrique Silva <diegosilvadhs@gmail.com>
2019-09-26 09:31:12 -0300

    Pagination

--- src/main/java/com/diegohenrique/course/repositories/ProductRepository.java
+++ src/main/java/com/diegohenrique/course/repositories/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.diegohenrique.course.repositories;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.diegohenrique.course.entities.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
 
 }
