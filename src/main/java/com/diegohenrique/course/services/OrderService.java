fd0c535b2c10bc5b1cc064b476a2caf66a893a99
Diego Henrique Silva <diegosilvadhs@gmail.com>
2019-11-03 11:28:12 -0300

    Order GET endpoints part1

--- src/main/java/com/diegohenrique/course/services/OrderService.java
+++ src/main/java/com/diegohenrique/course/services/OrderService.java
@@ -2,13 +2,17 @@ package com.diegohenrique.course.services;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.diegohenrique.course.dto.OrderDTO;
+import com.diegohenrique.course.dto.OrderItemDTO;
 import com.diegohenrique.course.entities.Order;
+import com.diegohenrique.course.entities.OrderItem;
 import com.diegohenrique.course.entities.User;
 import com.diegohenrique.course.repositories.OrderRepository;
 import com.diegohenrique.course.services.exceptions.ResourceNotFoundException;
@@ -42,5 +46,13 @@ public class OrderService {
 		User client = authService.authenticated();
 		List<Order> list = repository.findByClient(client);
 		return list.stream().map(e -> new OrderDTO(e)).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public List<OrderItemDTO> findItems(Long id) {
+		Order order = repository.getOne(id);
+		authService.validadeOwnOrderOrAdmin(order);
+		Set<OrderItem> set = order.getItems();
+		return set.stream().map(e -> new OrderItemDTO(e)).collect(Collectors.toList());
 	}
 }
