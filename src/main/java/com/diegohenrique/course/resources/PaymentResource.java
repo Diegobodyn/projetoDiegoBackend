76309fc0597e6a0b78e42a4638992db317145a24
Diego Henrique Silva <diegosilvadhs@gmail.com>
2019-11-03 15:32:19 -0300

    Payment PUT POST endpoints

--- src/main/java/com/diegohenrique/course/resources/PaymentResource.java
+++ src/main/java/com/diegohenrique/course/resources/PaymentResource.java
@@ -1,15 +1,22 @@
 package com.diegohenrique.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.diegohenrique.course.dto.PaymentDTO;
 import com.diegohenrique.course.dto.PaymentDTO;
 import com.diegohenrique.course.services.PaymentService;
 
@@ -32,6 +39,23 @@ public class PaymentResource {
 	public ResponseEntity<PaymentDTO> findById(@PathVariable  Long id){
 		PaymentDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	@PostMapping
+	public ResponseEntity<PaymentDTO> insert(@RequestBody PaymentDTO dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				  .buildAndExpand(dto.getId()).toUri() ;
+		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<PaymentDTO> update(@PathVariable Long id, @RequestBody PaymentDTO obj){
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
 	}
 	
 }
